3e4ae9fd9037b4898178e7eb114973354cd17614
cengiz <cengiz>
2005-06-15 20:07:56 +0000

    Cleaned-up, added documentation

---
+++ rc.sh
@@ -0,0 +1,4 @@
+
+JHOME=~jaegerlab
+export PATH=$JHOME/genesis-2.3.1a:$JHOME/brute_scripts:${PATH}
+
---
+++ sge_perllock_local.sh
@@ -0,0 +1,44 @@
+#! /bin/bash
+
+#$ -cwd
+#$ -j y
+#$ -N sge_run
+#$ -S /bin/bash
+
+# Run this with:
+# qsub -t 1:1310 ~/brute_scripts/sge_perllock.sh setup_cip_act_handtune.g blocked_gps0501-03_2.par
+
+# Need to source our own rc file. >:O
+source $HOME/.bashrc
+
+curdir=`pwd`
+
+echo "Starting job $SGE_TASK_ID/$SGE_TASK_LAST on $HOSTNAME"
+date
+
+if [ -z "$2" ]; then
+   echo "Need to specify GENESIS script and parameter file."
+   echo ""
+   echo "Usage: "
+   echo "   $0 genesis_script parameter_file"
+   exit -1
+fi
+
+trap exit INT
+
+genfile=$1
+parfile=$2
+
+export GENESIS_PAR_ROW
+
+# Read parameter values.
+GENESIS_PAR_ROW=`lockLinuxFile $parfile dosim $parfile`
+
+[ "$GENESIS_PAR_ROW" == "?" ] && echo "No more parameters, ending." && exit 0;
+
+# Run genesis 
+genesis -nox -batch -notty $genfile 
+
+echo "Ending job"
+date
+
