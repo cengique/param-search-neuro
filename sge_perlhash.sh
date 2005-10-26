8f85aad63e9b6c50f52d5f8714818363385665b4
cengiz <cengiz>
2005-10-26 17:29:26 +0000

    added new script and time genesis runs

--- sge_perlhash.sh
+++ sge_perlhash.sh
@@ -6,7 +6,7 @@
 #$ -S /bin/bash
 
 # The following skips work46 because of Perl DB_File problems
-## -q all.q@work01,all.q@work02,all.q@work03,all.q@work04,all.q@work05,all.q@work06,all.q@work07,all.q@work08,all.q@work09,all.q@work10,all.q@work11,all.q@work12,all.q@work13,all.q@work14,all.q@work15,all.q@work16,all.q@work17,all.q@work18,all.q@work19,all.q@work20,all.q@work21,all.q@work22,all.q@work23,all.q@work24,all.q@work25,all.q@work26,all.q@work27,all.q@work28,all.q@work29,all.q@work30,all.q@work31,all.q@work32,all.q@work33,all.q@work34,all.q@work35,all.q@work36,all.q@work37,all.q@work38,all.q@work39,all.q@work40,all.q@work41,all.q@work42,all.q@work43,all.q@work44,all.q@work45,all.q@work47,all.q@work48,all.q@work49,all.q@work50,all.q@work51,all.q@work52,all.q@work53,all.q@work54,all.q@work55,all.q@work56,all.q@work57,all.q@work58,all.q@work59,all.q@work60,all.q@work61,all.q@work62
+## -q all.q@work01,all.q@work02,all.q@work03,all.q@work04,all.q@work05,all.q@work06,all.q@work07,all.q@work08,all.q@work09,all.q@work10,all.q@work11,all.q@work12,all.q@work13,all.q@work14,all.q@work15,all.q@work16,all.q@work17,all.q@work18,all.q@work19,all.q@work20,all.q@work25,all.q@work26,all.q@work27,all.q@work28,all.q@work29,all.q@work30,all.q@work31,all.q@work32,all.q@work33,all.q@work34,all.q@work35,all.q@work36,all.q@work37,all.q@work38,all.q@work39,all.q@work40,all.q@work41,all.q@work42,all.q@work43,all.q@work44,all.q@work45,all.q@work47,all.q@work48,all.q@work49,all.q@work50,all.q@work51,all.q@work52,all.q@work53,all.q@work54,all.q@work55,all.q@work56,all.q@work57,all.q@work58,all.q@work59,all.q@work60,all.q@work61,all.q@work62
 
 # This sge job script reads a designated row from the parameter file and executes
 # genesis to process it. It uses a fast hashtable access to read the parameter 
@@ -18,7 +18,7 @@
 # race conditions that occur when writing to the parameter file concurrently.
 
 # Author: Cengiz Gunay <cgunay@emory.edu> 2005/06/29
-# $Id: sge_perlhash.sh,v 1.2 2005/07/06 00:19:27 cengiz Exp $
+# $Id: sge_perlhash.sh,v 1.3 2005/10/26 17:29:26 cengiz Exp $
 
 # Run this with:
 # qsub -t 1:1310 ~/brute_scripts/sge_perlhash.sh setup_cip_act_handtune.g blocked_gps0501-03_2.par
@@ -65,7 +65,7 @@ GENESIS_PAR_ROW=`dosimnum $parfile $SGE_TASK_ID`
 [ "$?" != "0" ] && echo "Cannot read parameter row $SGE_TASK_ID, ending." && exit -1;
 
 # Run genesis 
-genesis -nox -batch -notty $genfile
+time genesis -nox -batch -notty $genfile
 
 [ "$?" != "0" ] && echo "GENESIS run failed, terminating job!" && exit -1
 
