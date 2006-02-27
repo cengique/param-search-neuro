4e10a1a11d94fcfb6da821b87c91351de625bb63
cengiz <cengiz>
2006-02-27 16:47:17 +0000

    Added local software installation directory to path.

--- rc.sh
+++ rc.sh
@@ -1,4 +1,4 @@
 
 JHOME=~jaegerlab
-export PATH=$JHOME/genesis-2.3.1a:$JHOME/brute_scripts:/usr/local/MATLAB/bin:${PATH}:$JHOME/matlab/cengiz
+export PATH=$JHOME/genesis-2.3.1a:$JHOME/brute_scripts:/usr/local/MATLAB/bin:${PATH}:$JHOME/matlab/cengiz:$JHOME/sw/bin
 
