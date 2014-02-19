c2cb49b8121b172895abf62746440f4637e03111
Cengiz Gunay <cengique@users.sf.net>
2014-02-19 16:27:06 -0500

    anca's version with new parameter type as list of values

--- paramScanSingle.pl
+++ paramScanSingle.pl
@@ -12,7 +12,7 @@ use strict;
 
 my $filename_prefix = shift;
 
-my @params = @{&readParamFile};
+my @params = @{&readParamFile}; # read input ASCII file
 
 # Display data structure
 # Calculate total number of runs
@@ -25,9 +25,11 @@ foreach my $param (@params) {
 
 print "" . ($#params + 1) . " parameters. $steps total number of simulations.\n";
 
-# Count to steps and put into param file
 
+# Builds param file (*.par) containing a combination of simulation parameters per each line.
+# This file will be used to submit different jobs to different machines on cluster.
 my $filename = $filename_prefix . ".par";
+
 open OFILE, ">$filename" or die "Cannot open $filename for writing!\n";
 print OFILE "$steps " . ($#params + 1) . "\n";
 for (my $i = 0; $i < $steps; $i++ ) {
@@ -36,7 +38,9 @@ for (my $i = 0; $i < $steps; $i++ ) {
 
 close OFILE if tell(OFILE) != -1;
 
-# Parameter values according to a specific count
+
+# Returns parameter combination (one per easch line) from input ASCII file 
+# according to a specific count
 sub paramConf {
   my $count = shift;
   my $orig_count = $count;
@@ -52,13 +56,15 @@ sub paramConf {
 	$param_val = $param{range_low};
       } else {
 	$param_val = $param{range_low} + 
-	  $param_step * ($param{range_high} - $param{range_low}) / 
+	    $param_step * ($param{range_high} - $param{range_low}) / 
 	    ( $param{steps} - 1); # inclusive
       }
     } elsif (exists $param{base}) { # Geometric increment parameter
       $param_val = $param{base} * pow($param{mult}, $param_step);
     } elsif (exists $param{start}) { # counter
       $param_val = $param{start} + $orig_count;
+    } elsif (exists $param{list}) { # each parameter from given list [param1 param2 ... ]
+      $param_val = ${$param{list}}[$param_step];
     } else {
       die "Invalid parameter: $param\n";
     }
@@ -88,14 +94,24 @@ sub pow {
   $_ = exp($power * log($base));
 }
 
+# Main function for reading parameters from the given ASCII input file (*.txt).
+# Reads (and parses) one line at a time, and ends after reading the trial line.
+# There are 4 formats (at this time) accepted in the input file.Returns the structure.
+# For LEECH use this format for each line: param_name [param_val1 param_val2 ...]
 sub readParamFile {
   my @params;
 
   # Read std input
   while (<>) {
     next if /^\s*#/;		# Skip comment inputs.
-    if (/(\w+)\s+([\.\-\w]+)([\*\s]+)([\.\-\w]+)([\^\s]+)(\w+)/) {
-      print "Name: '$1', '$2'$3'$4'$5'$6'\n";
+    if ((/(\w+)\s+\[\s*([^\]]*)\s*\]/)) {
+      # choose from list
+      print "LIST: Name: '$1', list: '$2'\n";
+      my @list = split(/\s+/, $2);
+      push @params, { name => "$1", list => [ @list ],
+		      steps => $#list + 1 };
+    } elsif (/(\w+)\s+([\.\-\w]+)([\*\s]+)([\.\-\w]+)([\^\s]+)(\w+)/) {
+      print "RANGE Name: '$1', '$2'$3'$4'$5'$6'\n";
       my $name = $1;
       my $base = $2;
       my $top = $4;
@@ -112,10 +128,13 @@ sub readParamFile {
       }
     } elsif ((/(\w+)\s+([\.\-\w]+)\s*\+\+\s*([\.\-\w]*)/)) {
       # counter
-      print "Name: '$1', '$2'++ '$3'\n";
+      print "COUNTER Name: '$1', '$2'++ '$3'\n";
       push @params, { name => "$1", start => $2,
 		      increment => $3, steps => 1 };
+    } else {
+      print "Cannot recognize parameter line: '$_'\n";
     }
+
   }
 
   $_ = \@params;
@@ -136,10 +155,12 @@ sub usage {
  	param_name range_low range_high num_steps
  2. Multiplicative increments
  	param_name base_val *mul_factor ^num_steps
- 3. Independent counter (indicates trial number)
+ 3. Choose from a list
+	param_name [ val1 val2 ... ]
+ 4. Independent counter (indicates trial number)
 	param_name start_val ++ [increment]
 
- See the examples in the brute_scripts/paramRanges*.txt.
+ See the examples in paramRanges*.txt.
 
  The parameters are written with order of appearance to Genesis files.
 
