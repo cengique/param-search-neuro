c617fd5227124bf51b810d7695c09f672d5678d3
Cengiz Gunay <cengique@users.sf.net>
2014-02-19 16:09:44 -0500

    added docs and made it more general than lgenesis

--- sge_perlhash.sh
+++ sge_perlhash.sh
@@ -6,8 +6,7 @@
 #$ -S /bin/bash
 #$ -o sge_run/$HOSTNAME
 
-
-# This sge job script reads a designated row from the parameter file and executes
+# This SGE job script reads a designated row from the parameter file and executes
 # genesis to process it. It uses a fast hashtable access to read the parameter 
 # row from a database created with the create_perlhash_param_db script.
 # Notice that, this script does not mark the row as processed in the original 
@@ -16,16 +15,24 @@
 # The reason this script does not modify the parameter file is to avoid
 # race conditions that occur when writing to the parameter file concurrently.
 
+# To choose a different Genesis binary modify the $GENESIS environment variable.
+
 # Author: Cengiz Gunay <cgunay@emory.edu> 2005/06/29
 # $Id: sge_perlhash.sh,v 1.4 2006/02/27 16:47:46 cengiz Exp $
 
-# Run this with:
+# Modified by Anca Doloc-Mihu <adolocm@emory.edu>, 2008/08/14
+# switched to lgenesis (leech-genesis)
+
+# Example:
 # qsub -t 1:1310 ~/brute_scripts/sge_perlhash.sh setup_cip_act_handtune.g blocked_gps0501-03_2.par
 
 # Need to source our own rc file. >:O
 source $HOME/.bashrc
 
 curdir=`pwd`
+
+# Default value is genesis
+${GENESIS:=genesis}
 
 echo -n "Starting job $SGE_TASK_ID/$SGE_TASK_LAST from parameter file $2 on $HOSTNAME at "
 date
@@ -43,20 +50,7 @@ trap exit INT
 genfile=$1
 parfile=$2
 
-# Special check for cluster traps
-#if [ $HOSTNAME == "work46" ]; then 
-#	echo "I don't like work46, it doesn't have the perl libraries I need. I'm going to take a nap now."
-#	sleep $[60 * 60 * 24 * 3]	# Sleep for 3 days
-#	exit -1
-#fi
-
 export GENESIS_PAR_ROW
-
-# Random delay to avoid deadlock only for the first batch of nodes
-# Afterwards, the offsets should be preserved.
-#if [[ $SGE_TASK_ID < 126 ]]; then
-#   awk 'BEGIN {system("sleep " rand() * 20)}'
-#fi
 
 # Read parameter values.
 GENESIS_PAR_ROW=`dosimnum $parfile $SGE_TASK_ID`
@@ -64,7 +58,7 @@ GENESIS_PAR_ROW=`dosimnum $parfile $SGE_TASK_ID`
 [ "$?" != "0" ] && echo "Cannot read parameter row $SGE_TASK_ID, ending." && exit -1;
 
 # Run genesis 
-/usr/bin/time -f  "=== Time of simulation: elapsed = %E...kernel cpu = %S... user cpu = %U... cpu alloc = %P ====" lgenesis -nox -batch -notty $genfile
+/usr/bin/time -f  "=== Time of simulation: elapsed = %E...kernel cpu = %S... user cpu = %U... cpu alloc = %P ====" $GENESIS -nox -batch -notty $genfile
 
 [ "$?" != "0" ] && echo "GENESIS run failed, terminating job!" && exit -1
 
